31f3b354c62658dba2c7c7d5259a069f10f80d53
Charly Cimino <carlosecimino@gmail.com>
2022-07-13 14:04:33 -0300

    CRUD finalizado

--- src/main/java/cac/crud22033/modelo/ModeloMySQL.java
+++ src/main/java/cac/crud22033/modelo/ModeloMySQL.java
@@ -6,8 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -19,6 +17,9 @@ public class ModeloMySQL implements Modelo {
 
     private static final String GET_ALL_QUERY = "SELECT * FROM alumnos";
     private static final String GET_BY_ID_QUERY = "SELECT * FROM alumnos WHERE id_alumno = ?";
+    private static final String ADD_QUERY = "INSERT INTO alumnos VALUES (null, ?, ?, ?, ?, ?)";
+    private static final String UPDATE_QUERY = "UPDATE alumnos SET nombre = ?, apellido = ?, fechaNac = ?, mail = ?, fotoBase64 = ? WHERE id_alumno = ?";
+    private static final String DELETE_QUERY = "DELETE FROM alumnos WHERE id_alumno = ?";
 
     @Override
     public List<Alumno> getAlumnos() {
@@ -29,6 +30,8 @@ public class ModeloMySQL implements Modelo {
             }
         } catch (SQLException ex) {
             throw new RuntimeException("Error de SQL", ex);
+        } catch (Exception ex) {
+            throw new RuntimeException("Error al obtener alumnos", ex);
         }
         return alumnos;
     }
@@ -44,23 +47,61 @@ public class ModeloMySQL implements Modelo {
             }
         } catch (SQLException ex) {
             throw new RuntimeException("Error de SQL", ex);
+        } catch (Exception ex) {
+            throw new RuntimeException("Error al obtener alumnos", ex);
         }
         return alu;
     }
 
     @Override
     public int addAlumno(Alumno alumno) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int regsAgregados = 0;
+        try ( Connection con = Conexion.getConnection();  PreparedStatement ps = con.prepareStatement(ADD_QUERY);) {
+            fillPreparedStatement(ps, alumno);
+            regsAgregados = ps.executeUpdate();
+        } catch (SQLException ex) {
+            throw new RuntimeException("Error de SQL", ex);
+        } catch (Exception ex) {
+            throw new RuntimeException("Error al obtener alumnos", ex);
+        }
+        return regsAgregados;
     }
 
     @Override
     public int updateAlumno(Alumno alumno) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int regsModificados = 0;
+        try ( Connection con = Conexion.getConnection();  PreparedStatement ps = con.prepareStatement(UPDATE_QUERY);) {
+            fillPreparedStatement(ps, alumno);
+            ps.setInt(6, alumno.getId());
+            regsModificados = ps.executeUpdate();
+        } catch (SQLException ex) {
+            throw new RuntimeException("Error de SQL", ex);
+        } catch (Exception ex) {
+            throw new RuntimeException("Error al obtener alumnos", ex);
+        }
+        return regsModificados;
     }
 
     @Override
     public int removeAlumno(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int regsBorrados = 0;
+        try ( Connection con = Conexion.getConnection();  PreparedStatement ps = con.prepareStatement(DELETE_QUERY);) {
+            ps.setInt(1, id);
+            regsBorrados = ps.executeUpdate();
+        } catch (SQLException ex) {
+            throw new RuntimeException("Error de SQL", ex);
+        } catch (Exception ex) {
+            throw new RuntimeException("Error al obtener alumnos", ex);
+        }
+        return regsBorrados;
+    }
+
+    private void fillPreparedStatement(PreparedStatement ps, Alumno alumno) throws SQLException {
+        ps.setString(1, alumno.getNombre());
+        ps.setString(2, alumno.getApellido());
+        ps.setString(3, alumno.getFechaNacimiento());
+        ps.setString(4, alumno.getMail());
+        ps.setString(5, alumno.getFoto());
     }
 
     private Alumno rsToAlumno(ResultSet rs) throws SQLException {
